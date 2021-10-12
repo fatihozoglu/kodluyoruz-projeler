e2832b2c887879db418edfe79b4ec45d933a7112
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-10-12 22:06:09 +0300

    Add Nodejs_odev_3

---
+++ Nodejs_odev_3/circle.js
@@ -0,0 +1,7 @@
+module.exports.circleArea = function (r) {
+    console.log(Math.PI * ( r ** 2));
+}
+
+module.exports.circleCircumference = function (r) {
+    console.log(2 * Math.PI * r);
+}
---
+++ Nodejs_odev_3/require.js
@@ -0,0 +1,4 @@
+const importedObjects = require("./circle.js")
+
+importedObjects.circleArea(5);
+importedObjects.circleCircumference(5);
