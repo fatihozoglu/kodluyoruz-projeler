02b5506b1773c67203e61cd1ff12293cd0d1297a
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-18 13:28:47 +0300

    Complete Nodejs Homework 9 Requirements

--- Nodejs_clean_blog/app.js
+++ Nodejs_clean_blog/app.js
@@ -1,27 +1,56 @@
 const express = require("express");
-const ejs = require("ejs");
+const Post = require("./models/Post");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Connect to MongoDB Database
+mongoose.connect("mongodb://127.0.0.1:27017/clean-blog-db");
+
 // Setting ejs as a view engine
 app.set("view engine", "ejs");
-// Using express.static middleware for our static files to be served from public folder
+// Using express.static middleware for our static files to be served from 'public' folder
 app.use(express.static("public"));
+// Using express.urlencoded middleware to encode the url data and parse it
+app.use(express.urlencoded({ extended: true }));
+// Using express.json() midlleware to get the data and parse it as json
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.render("index");
+// Home Page
+app.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.render("index", {
+      posts,
+    });
+  } catch (err) {
+    console.log(err.messsage);
+  }
 });
 
+// About Page
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
+// Add Post Page
 app.get("/add_post", (req, res) => {
   res.render("add_post");
 });
 
+// Individual Pages for each Post
 app.get("/post", (req, res) => {
   res.render("post");
+});
+
+// Route for getting the data sent from Add Post Form
+app.post("/posts", async (req, res) => {
+  try {
+    await Post.create(req.body);
+  } catch (err) {
+    console.log(err.message);
+  }
+  res.redirect("/");
 });
 
 app.listen(8080, () => {
---
+++ Nodejs_clean_blog/mongoose_crud.js
@@ -0,0 +1,32 @@
+// Create a Blog Post Data
+//   Post.create({
+//     name: "Second Blog Post",
+//     content:
+//       "This is the second blog post data for our Clean Blog Project in Patika.dev",
+//     keywords: ["mongoose", "express", "nodejs"],
+//   });
+
+// Read a Blog Post data from Database
+//   Post.find({}, (err, data) => {
+//     if (err) console.log(err.message);
+//     else console.log(data);
+//   });
+
+// Update a Blog Post data in Database
+//   Post.findByIdAndUpdate(
+//     "6195254432f730bcdd2d6054",
+//     {
+//       name: "Post name Updated",
+//       content: "This is the updated blog post for our Clean Blog Project",
+//     },
+//     (err, data) => {
+//       if (err) console.log(err.message);
+//       else console.log(data);
+//     }
+//   );
+
+// Delete a Blog Post from Database
+//   Post.findByIdAndDelete("6195254432f730bcdd2d6054", (err, data) => {
+//     if (err) console.log(err.message);
+//     else console.log("Blog Post Deleted");
+//   });
